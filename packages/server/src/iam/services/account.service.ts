30bcc7c87614a15e5d622c9a95ddadda0cb8db7a
Michael McMinn <mmcminn@uniondynamic.us>
2026-01-18 01:24:11 +0000

    Making an account master object, will want to split this down later but having it all here isolates the functions better and reduces the scope the AI needs to consider when genereating implementations.

--- packages/server/src/iam/services/account.service.ts
+++ packages/server/src/iam/services/account.service.ts
@@ -1,20 +1,39 @@
+import { AccountDescriptorInput, IAccountDescriptor } from '../Interface.Iam'
+
 export interface IAccountService {
-    registerUser(payload: any): Promise<any>
-    acceptInvite(token: string, payload: any): Promise<any>
+    createUser(payload: AccountDescriptorInput): Promise<any>
+    registerUser(payload: AccountDescriptorInput): Promise<any>
+    createInvite(payload: AccountDescriptorInput): Promise<any>
+    revokeInvite(inviteId: string): Promise<void>
+    acceptInvite(token: string, payload: AccountDescriptorInput): Promise<any>
     resendInvite(email: string): Promise<void>
     sendVerificationEmail(email: string): Promise<void>
     verifyEmail(token: string): Promise<void>
     resendVerificationEmail(email: string): Promise<void>
-    getProfile(userId: string): Promise<any>
-    updateProfile(userId: string, payload: any): Promise<any>
+    getProfile(userId: string): Promise<IAccountDescriptor | null>
+    updateProfile(userId: string, payload: AccountDescriptorInput): Promise<IAccountDescriptor | null>
+    deactivateAccount(userId: string): Promise<void>
+    deleteAccount(userId: string): Promise<void>
 }
 
 export class AccountService implements IAccountService {
-    async registerUser(_payload: any): Promise<any> {
+    async createUser(_payload: AccountDescriptorInput): Promise<any> {
         return null
     }
 
-    async acceptInvite(_token: string, _payload: any): Promise<any> {
+    async registerUser(_payload: AccountDescriptorInput): Promise<any> {
+        return null
+    }
+
+    async createInvite(_payload: AccountDescriptorInput): Promise<any> {
+        return null
+    }
+
+    async revokeInvite(_inviteId: string): Promise<void> {
+        return
+    }
+
+    async acceptInvite(_token: string, _payload: AccountDescriptorInput): Promise<any> {
         return null
     }
 
@@ -34,12 +53,20 @@ export class AccountService implements IAccountService {
         return
     }
 
-    async getProfile(_userId: string): Promise<any> {
+    async getProfile(_userId: string): Promise<IAccountDescriptor | null> {
         return null
     }
 
-    async updateProfile(_userId: string, _payload: any): Promise<any> {
+    async updateProfile(_userId: string, _payload: AccountDescriptorInput): Promise<IAccountDescriptor | null> {
         return null
+    }
+
+    async deactivateAccount(_userId: string): Promise<void> {
+        return
+    }
+
+    async deleteAccount(_userId: string): Promise<void> {
+        return
     }
 }
 export default AccountService
