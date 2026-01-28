3ca8d172e902fc9ef7319ee77b15c417476e9b1e
Michael McMinn <mmcminn@uniondynamic.us>
2026-01-28 06:03:24 +0000

    Adding licnese headers

--- packages/server/src/iam/sso/SSOBase.ts
+++ packages/server/src/iam/sso/SSOBase.ts
@@ -1,3 +1,17 @@
+/*
+ * Copyright (c) 2026 Union Dynamic, Inc
+ *
+ * This source code is licensed under the "IAM Module License" (AGPLv3 + AI Clause).
+ * You may not use this file except in compliance with the License.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the LICENSE file in this directory for the full license text and
+ * restrictions regarding Artificial Intelligence training.
+ */
+
 export default class SSOBase {
     protected config: any
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
